f16fabb0eb931479cc18360781a072b214d14ae8
Alena <kosoohina@yandex.ru>
2022-12-21 14:36:34 +0300

    Work

---
+++ PZ_9/PZ_9/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите натуральное число:");
+int chislo = int.Parse(Console.ReadLine());
+if (chislo < 0) { Console.Write($"{chislo} не натуральное число"); }
+else { Counter(chislo); }
+void Counter(int chislo)
+{
+    if (chislo == 0) return;
+    Console.Write($"{chislo} ");
+    Counter(chislo - 1);
+}
---
+++ PZ_9/Task_66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите M:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите N:");
+int n = int.Parse(Console.ReadLine());
+Summa(m, n, 0);
+void Summa(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"Сумма элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (m++);
+    Summa(m, n, sum);
+}
